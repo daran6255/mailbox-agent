e886e20c1f002c79e28d317577255d1c12bd0f9b
daran6255 <darandharani6255@gmail.com>
2025-07-11 16:20:03 +0530

    modifyed cron

--- check_pm2.sh
+++ check_pm2.sh
@@ -1,29 +1,13 @@
-# #!/bin/bash
-# echo "[DEBUG] Running PM2 check..." >> /home/winvinaya/mailbox-agent/check_pm2.log
-# date >> /home/winvinaya/mailbox-agent/check_pm2.log
-
-# if ! pm2 list | grep -q "mail-agent"; then
-#     echo "[DEBUG] mail-agent not running, restarting..." >> /home/winvinaya/mailbox-agent/check_pm2.log
-#     pm2 start /home/winvinaya/mailbox-agent/start_agent.sh --name mail-agent
-#     echo "[DEBUG] Sending alert email..." >> /home/winvinaya/mailbox-agent/check_pm2.log
-
-#     python3 /home/winvinaya/mailbox-agent/send_error_mail.py "mail-agent was not running and was restarted"
-# else
-#     echo "[DEBUG] mail-agent is running." >> /home/winvinaya/mailbox-agent/check_pm2.log
-# fi
-
 #!/bin/bash
-echo "[DEBUG] Running PM2 check..."
+echo "[DEBUG] Running PM2 check..." >> /home/winvinaya/mailbox-agent/check_pm2.log
+date >> /home/winvinaya/mailbox-agent/check_pm2.log
 
-# Force simulate failure:
-# if ! pm2 list | grep -q mail-agent; then
-true
-if [ $? -ne 0 ]; then
-    echo "[DEBUG] mail-agent not running, restarting..."
+if ! pm2 list | grep -q "mail-agent"; then
+    echo "[DEBUG] mail-agent not running, restarting..." >> /home/winvinaya/mailbox-agent/check_pm2.log
     pm2 start /home/winvinaya/mailbox-agent/start_agent.sh --name mail-agent
-    echo "[DEBUG] Sending alert email..."
+    echo "[DEBUG] Sending alert email..." >> /home/winvinaya/mailbox-agent/check_pm2.log
 
     python3 /home/winvinaya/mailbox-agent/send_error_mail.py "mail-agent was not running and was restarted"
 else
-    echo "[DEBUG] mail-agent is running."
+    echo "[DEBUG] mail-agent is running." >> /home/winvinaya/mailbox-agent/check_pm2.log
 fi
